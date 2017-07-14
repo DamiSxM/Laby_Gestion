1272640984f0cca5c99e055e06dc4abac2dd9947
Dami <dami.bachelet@gmail.com>
2017-07-15 00:33:21 +0200

    fin de journée
    - ajout de vitesse dans gestion
    - ajout control déplacement
    - ajout control de vitesse

--- Laby_Gestion/Gestion.cs
+++ Laby_Gestion/Gestion.cs
@@ -2,10 +2,7 @@
 using System.Collections;
 using System.Drawing;
 using System.Runtime.Serialization;
-/*using Laby_Interfaces;
-using Laby_Maze;*/
 
-//namespace Laby_Gestion
 namespace Labyrinthe
 {
     [Serializable]
@@ -50,8 +47,14 @@ namespace Labyrinthe
         IAffichage _affichage;
         ILiaison _liaison;
 
-        int _perso_vitesse = 2;
+        int _cellSize;
+        int _persoVitesse = 2;
+        Point _positionCase;
+        Point _positionPixel;
+
         Hashtable _items = new Hashtable();
+
+        public int PersoVitesse { get { return _persoVitesse; } set { _persoVitesse = value; } }
 
         public Gestion(Maze labyrinthe, IAffichage affichage, ILiaison liaison)
         {
@@ -60,6 +63,7 @@ namespace Labyrinthe
             _liaison = liaison;
 
             _affichage.PositionChanged += PositionChanged;
+            _cellSize = _affichage.GetCellSize();
 
             _liaison.DataReceived += DataReceived;
             _liaison.ClientConnected += ClientConnected;
@@ -93,7 +97,10 @@ namespace Labyrinthe
             } while (_labyrinthe.Labyrinthe[x, y] != 0);
 
             System.Diagnostics.Debug.WriteLine(string.Format("{0} : PersoRandomPosition : X {1}, Y {2}", IsServer(), x, y));
-            PersoTeleport(new Point(x, y));
+            _positionCase = new Point(x, y);
+            _positionPixel = new Point(x * _cellSize, y * _cellSize);
+
+            PersoTeleport(_positionCase);
         }
 
         void GenerationItems()
@@ -136,13 +143,10 @@ namespace Labyrinthe
         private void PositionChanged(int x, int y)
         {
             System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.PositionChanged : X {1}, Y {2}", IsServer(), x, y));
-            //System.Diagnostics.Debug.WriteLine(string.Format("PositionChanged : {0}", _labyrinthe.Labyrinthe[x, y]));
             if (_liaison.IsServer()) // Si on est le server
             {
                 ObjectInstruction data = new ObjectInstruction(new Point(x, y), "player", "move");
-                _liaison.SendData(data);
-                //DataPosition dp = new DataPosition("player", new Point(x, y));
-                //_liaison.SendData(dp); // Si il y a des clients, on leur envoi sa position
+                _liaison.SendData(data); // Si il y a des clients, on leur envoi sa position
                 if (_items.Contains(new Point(x, y)))
                 {
                     System.Diagnostics.Debug.WriteLine(string.Format("{0} : ITEM !", IsServer()));
@@ -152,9 +156,7 @@ namespace Labyrinthe
             else // Si on est un client
             {
                 ObjectInstruction data = new ObjectInstruction(new Point(x, y), "player", "move");
-                _liaison.SendData(data);
-                //DataPosition dp = new DataPosition("player", new Point(x, y));
-                //_liaison.SendData(dp); // On leur envoi sa position au server
+                _liaison.SendData(data); // On leur envoi sa position au server
             }
         }
 
@@ -171,7 +173,7 @@ namespace Labyrinthe
             {
                 _affichage.Debug("CLIENT");
 
-                _liaison.SendData(new DataPosition("player", _affichage.PersoGetPosition())); // Envoi position client au server
+                _liaison.SendData(new ObjectInstruction(_affichage.PersoGetPosition(), "player", "move"));
                 _affichage.Warfog(4);
             }
         }
@@ -195,8 +197,6 @@ namespace Labyrinthe
 
         private void ReceptionObjectInstruction(string sender, ObjectInstruction data)
         {
-            System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionObjectInstruction", IsServer()));
-
             if (data.Type == "item")
             {
                 if (data.Instruction == "add")
@@ -207,11 +207,9 @@ namespace Labyrinthe
                 }
                 else if (data.Instruction == "remove")
                 {
-                    System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionObjectInstruction : Item remove", IsServer()));
-
-                    ItemRemove((Point)data.Data);
-                    //_items.Remove((Point)data.Data);
-                    //_affichage.ItemRemove((Point)data.Data);
+                    Point p = (Point)data.Data;
+                    System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionObjectInstruction : Item remove : X {1}, Y {2}", IsServer(), p.X, p.Y));
+                    ItemRemove(p);
                 }
             }
             else if (data.Type == "player")
@@ -226,7 +224,7 @@ namespace Labyrinthe
 
         void ReceptionLabyrinthe(int[,] lab)
         {
-            System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionLabyrinthe : Nouveau Labyrinthe !", IsServer()));
+            System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionLabyrinthe : Nouveau Labyrinthe ! x{1} y{1}", IsServer(), lab.GetLength(0)));
             _labyrinthe.Labyrinthe = lab;
             _affichage.LabyUpdate();
             PersoRandomPosition();
@@ -238,39 +236,82 @@ namespace Labyrinthe
         }
         void ReceptionPlayer(string ip, Point p)
         {
-            System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionPlayer : Perso[{1},{2}] Player[{3},{4}]", IsServer(), _affichage.PersoGetPosition().X, _affichage.PersoGetPosition().Y, p.X, p.Y));
 
-
-
-            if (_affichage.PlayerExists(ip))
+            if (_affichage.PlayerExists(ip)) // Si le player existe déjà : move
             {
-                System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionPlayer : {1} existe, move x{2}, y{3}", IsServer(), ip, p.X, p.Y));
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionPlayer : Perso[{1},{2}], PlayerMove[{3},{4}] -> {5}", IsServer(), _affichage.PersoGetPosition().X, _affichage.PersoGetPosition().Y, p.X, p.Y, ip));
                 _affichage.PlayerMove(ip, p);
 
-                if (_items.Contains(p))
-                {
-                    System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionPlayer : ITEM ! {1}", IsServer(), _items.Contains(p)));
-                    ItemRemove(p);
-                    /*_items.Remove(p);
-                    _affichage.ItemRemove(p);*/
-                }
-
-                if (p == _affichage.PersoGetPosition())
-                {
-                    System.Diagnostics.Debug.WriteLine(string.Format("{0} : FIGHT !", IsServer()));
-                }
             }
-            else
+            else // Si le player n'existe pas déjà : add
             {
-                System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionPlayer : {1} existe pas, add x{2}, y{3}", IsServer(), ip, p.X, p.Y));
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionPlayer : Perso[{1},{2}] PlayerAdd[{3},{4}] -> {5}", IsServer(), _affichage.PersoGetPosition().X, _affichage.PersoGetPosition().Y, p.X, p.Y, ip));
                 _affichage.PlayerAdd(ip, p);
+            }
 
+            if (_items.Contains(p)) // Si le player se déplace sur un objet :
+            {
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ReceptionPlayer : ITEM !", IsServer()));
+                ItemRemove(p);
+            }
+            if (p == _affichage.PersoGetPosition()) // Si le player se déplace sur le joueur :
+            {
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} : FIGHT !", IsServer()));
             }
         }
 
         public void PersoMove(Direction d)
         {
-            _affichage.PersoMove(d, _perso_vitesse);
+            // GetPersoPosition
+            Point currentPositionPixel = _affichage.PersoGetPositionPixel();
+
+            int x = 0, y = 0;
+            switch (d)
+            {
+                case Direction.LEFT:
+                    x -= _persoVitesse;
+                    break;
+                case Direction.UP:
+                    y -= _persoVitesse;
+                    break;
+                case Direction.RIGHT:
+                    x += _persoVitesse;
+                    break;
+                case Direction.DOWN:
+                    y += _persoVitesse;
+                    break;
+            }
+
+            Point newPosition = new Point(currentPositionPixel.X + x, currentPositionPixel.Y + y);
+            Point tmp = new Point((currentPositionPixel.X + x) / _cellSize, (currentPositionPixel.Y + y) / _cellSize);
+            Point tmpDown = new Point((currentPositionPixel.X + x) / _cellSize, (currentPositionPixel.Y + y + _cellSize/2) / _cellSize);
+
+            int labyCase = _labyrinthe.Labyrinthe[tmp.X, tmp.Y];
+            int labyCaseDown = _labyrinthe.Labyrinthe[tmp.X, tmpDown.Y];
+            switch (d)
+            {
+                case Direction.LEFT:
+                    if (labyCase != 0)
+                        newPosition.X = (tmp.X+1) * _cellSize;
+                    break;
+
+                case Direction.UP:
+                    if (labyCase != 0)
+                        newPosition.Y = (tmp.Y+1) * _cellSize;
+                    break;
+
+                case Direction.RIGHT:
+                    if (labyCase != 0)
+                        newPosition.X = tmp.X * _cellSize;
+                    break;
+
+                case Direction.DOWN:
+                    if (labyCaseDown != 0)
+                        newPosition.Y = tmpDown.Y * _cellSize - _cellSize / 2;
+                    break;
+            }
+            
+            _affichage.PersoMove(d, newPosition);
         }
         public void PersoTeleport(Point p)
         {
@@ -278,11 +319,11 @@ namespace Labyrinthe
 
             if (!_liaison.IsServer())
             {
-                _liaison.SendData(new DataPosition("player", _affichage.PersoGetPosition()));
+                _liaison.SendData(new ObjectInstruction(p, "player", "move"));
             }
             else if (_liaison.GetClientsCount() > 0)
             {
-                _liaison.SendData(new DataPosition("player", _affichage.PersoGetPosition()));
+                _liaison.SendData(new ObjectInstruction(p, "player", "move"));
             }
         }
 
@@ -315,20 +356,12 @@ namespace Labyrinthe
             _items.Remove(p);
             _affichage.ItemRemove(p);
 
-
             if (_liaison.IsServer())
             {
                 ObjectInstruction data = new ObjectInstruction(p, "item", "remove");
                 _liaison.SendData(data);
-                System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ItemRemove {1} : Envoi", IsServer(), p));
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ItemRemove {1} : Envoi aux clients", IsServer(), p));
             }
-
-            /*if (_liaison.IsFinRechercheServer())
-            {
-                ObjectInstruction data = new ObjectInstruction(p, "item", "remove");
-                _liaison.SendData(data);
-                System.Diagnostics.Debug.WriteLine(string.Format("{0} : Gestion.ItemRemove {1} : Envoi", IsServer(), p));
-            }*/
         }
     }
 }
